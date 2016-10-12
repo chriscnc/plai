0063e5e2b6a631a2b5c6683db18a1d030901c389
Chris <chris.cornelison@gmail.com>
2016-10-11 19:03:15 -0500

    completed chapter 5

--- parser-test.rkt
+++ parser-test.rkt
@@ -3,9 +3,18 @@
 (require rackunit
 	 "parser.rkt")
 
-(check-equal? 42 (interp (parse '42)))
-(check-equal? 3 (interp (parse '(+ 1 2))))
-(check-equal? 2 (interp (parse '(* 1 2))))
-(check-equal? 3 (interp (parse '(+ (* 1 2) 1))))
-(check-equal? 7 (interp (parse '(+ (* 1 2) (+ 2 3)))))
+(define fds 
+  (list
+    (fdC 'double 'x (plusC (idC 'x) (idC 'x)))
+    (fdC 'quadruple 'x (appC 'double (appC 'double (idC 'x))))
+    (fdC 'const5 '_ (numC 5))))
 
+(check-equal? 42 (interp (parse '42) fds))
+(check-equal? 3 (interp (parse '(+ 1 2)) fds))
+(check-equal? 2 (interp (parse '(* 1 2)) fds))
+(check-equal? 3 (interp (parse '(+ (* 1 2) 1)) fds))
+(check-equal? 7 (interp (parse '(+ (* 1 2) (+ 2 3))) fds))
+
+(check-equal? 7 (interp (parse '(+ 1 (double 3))) fds))
+(check-equal? 6 (interp (parse '(+ 1 (const5 _))) fds))
+(check-equal? 17 (interp (parse '(+ 1 (quadruple 4))) fds))
--- parser.rkt
+++ parser.rkt
@@ -1,27 +1,61 @@
 #lang plai-typed
 
-(define-type ArithC
-  [numC (n : number)]
-  [plusC (l : ArithC) (r : ArithC)]
-  [multC (l : ArithC) (r : ArithC)])
+(define-type ExprC
+  [numC  (n : number)]
+  [idC   (s : symbol)]
+  [appC  (fun : symbol) (arg : ExprC)]
+  [plusC (l : ExprC) (r : ExprC)]
+  [multC (l : ExprC) (r : ExprC)])
 
 
-(define (parse [s : s-expression]) : ArithC
+(define-type FunDefC
+  [fdC (name : symbol) (arg : symbol) (body : ExprC)])
+
+
+(define (parse [s : s-expression]) : ExprC
   (cond
     [(s-exp-number? s) (numC (s-exp->number s))]
+    [(s-exp-symbol? s) (idC (s-exp->symbol s))]
     [(s-exp-list? s)
      (let ([sl (s-exp->list s)])
        (case (s-exp->symbol (first sl))
          [(+) (plusC (parse (second sl)) (parse (third sl)))]
          [(*) (multC (parse (second sl)) (parse (third sl)))]
-         [else (error 'parse "invalid list input")]))]
+         [else (appC (s-exp->symbol (first sl)) (parse (second sl)))]))]
     [else (error 'parse "invalid input")]))
 
 
-(define (interp [a : ArithC]) : number
-  (type-case ArithC a
-    [numC (n) n]
-    [plusC (l r) (+ (interp l) (interp r))]
-    [multC (l r) (* (interp l) (interp r))]))
+(define (interp [e : ExprC] [fds : (listof FunDefC)]) : number
+  (type-case ExprC e
+    [numC  (n) n]
+    [idC   (s) (error 'interp "shouldn't get here")]
+    [appC  (f a) (local ([define fd (get-fundef f fds)])
+		   (interp (subst a 
+				  (fdC-arg fd)
+				  (fdC-body fd))
+			   fds))]
+    [plusC (l r) (+ (interp l fds) (interp r fds))]
+    [multC (l r) (* (interp l fds) (interp r fds))]))
+
+
+(define (get-fundef [n : symbol] [fds : (listof FunDefC)]) : FunDefC
+  (cond
+    [(empty? fds) (error 'get-fundef "reference to undefined function")]
+    [(cons? fds)  (cond
+		    [(equal? n (fdC-name (first fds))) (first fds)]
+		    [else (get-fundef n (rest fds))])]))
+
+
+(define (subst [what : ExprC] [for : symbol] [in : ExprC]) : ExprC
+  (type-case ExprC in
+	     [numC (n) in]
+	     [idC  (s) (cond
+			 [(symbol=? s for) what]
+			 [else in])]
+	     [appC (f a) (appC f (subst what for a))]
+	     [plusC (l r) (plusC (subst what for l) 
+				 (subst what for r))]
+	     [multC (l r) (multC (subst what for l) 
+				 (subst what for r))]))
 
 
