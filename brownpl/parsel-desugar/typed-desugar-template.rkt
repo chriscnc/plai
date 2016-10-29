14768a9223fa2eb20e84fcd3581da4bb8e8d89cb
Chris <chris.cornelison@gmail.com>
2016-10-28 21:41:18 -0500

    work on desugaring

--- brownpl/parsel-desugar/typed-desugar-template.rkt
+++ brownpl/parsel-desugar/typed-desugar-template.rkt
@@ -31,6 +31,19 @@
 (define (map-subtract (exprs : (listof ExprC))) : ExprC
   (foldl (lambda (expr result) (Prim2C 'num- result expr)) (first exprs) (rest exprs)))
 
+;; map-add-num builds an expression that maps 'num+ over a list of expressions
+(define (map-add-num (exprs : (listof ExprC))) : ExprC
+  (foldl (lambda (expr result) (Prim2C 'num+ result expr)) (first exprs) (rest exprs)))
+
+;; map-add-str builds an expression that maps 'string+ over a list of expressions
+(define (map-add-str (exprs : (listof ExprC))) : ExprC
+  (foldl (lambda (expr result) (Prim2C 'string+ result expr)) (first exprs) (rest exprs)))
+
+(define (desugar-seq (es : (listof ExprP))): ExprC
+  (let ([desugared-es (map desugar es)])
+    (foldl (lambda (expr result) (SeqC result expr))
+	   (first desugared-es)
+	   (rest desugared-es))))
 
 (define (desugar-subtract (args : (listof ExprP))) : ExprC
   (local ([define ids (make-ids (length args))]
@@ -40,17 +53,73 @@
            (map-subtract id-exps)
            (ErrorC (StrC "Bad arguments to -"))))))
 
+(define (desugar-add (args : (listof ExprP))) : ExprC
+  (local ([define ids (make-ids (length args))]
+	  [define id-exps (map IdC ids)])
+    (cascade-lets ids (map desugar args)
+      (IfC (all (map (lambda (e) (check-type e "number")) id-exps))
+	   (map-add-num id-exps)
+	   (IfC (all (map (lambda (e) (check-type e "string")) id-exps))
+		(map-add-str id-exps)
+		(ErrorC (StrC "Bad arguments to -")))))))
+
+(define (desugar-gt (args : (listof ExprP))) : ExprC
+  (let ([a1 (desugar (first args))]
+	[a2 (desugar (second args))])
+    (IfC (and (check-type a1 "number")
+	      (check-type a2 "number"))
+	 (Prim2C '> a1 a2)
+	 (ErrorC (StrC "Bad arguments to >")))))
+
+(define (desugar-lt (args : (listof ExprP))) : ExprC
+  (let ([a1 (desugar (first args))]
+	[a2 (desugar (second args))])
+    (IfC (and (check-type a1 "number")
+	      (check-type a2 "number"))
+	 (Prim2C '< a1 a2)
+	 (ErrorC (StrC "Bad arguments to <")))))
+
 (define (desugar (exprP : ExprP)) : ExprC
   (type-case ExprP exprP
 
     [NumP (n) (NumC n)]
     ;; Fill in more cases here...
+    [StrP (s) (StrC s)]
+    [TrueP () (TrueC)]
+    [FalseP () (FalseC)]
+    [IdP (name) (IdC name)]
+    [IfP (c t e) (IfC (desugar c) (desugar t) (desugar e))]
 
     [PrimP (op args)
         (case op
           ['- (cond
                 [(= 0 (length args)) (ErrorC (StrC "Empty list for prim op"))]
-                [(< 0 (length args)) (desugar-subtract args)])])]
+                [(< 0 (length args)) (desugar-subtract args)])]
+	  ['+ (cond
+		[(= 0 (length args)) (ErrorC (StrC "Empty list for prim op"))]
+		[(< 0 (length args)) (desugar-add args)])]
+	  ['== (cond
+		 [(= 0 (length args)) (ErrorC (StrC "Empty list for prim op"))]
+		 [(not (= 2 (length args))) (ErrorC (StrC "Bad primop"))]
+		 [else (Prim2C '== 
+			       (desugar (first args))
+			       (desugar (second args)))])]
+	  ['< (cond
+		 [(= 0 (length args)) (ErrorC (StrC "Empty list for prim op"))]
+		 [(not (= 2 (length args))) (ErrorC (StrC "Bad primop"))]
+		 [else (desugar-lt args)])]
+	  ['> (cond
+		 [(= 0 (length args)) (ErrorC (StrC "Empty list for prim op"))]
+		 [(not (= 2 (length args))) (ErrorC (StrC "Bad primop"))]
+		 [else (desugar-gt args)])]
+	  ['print (cond
+		    [(= 0 (length args)) (ErrorC (StrC "Empty list for prim op"))]
+		    [(not (= 1 (length args))) (ErrorC (StrC "Bad primop"))]
+		    [else (Prim1C 'print (desugar (first args)))])]
+	  )]
+    [SeqP (es) (desugar-seq es)]
+		 
+
 
     [WhileP (test body)
           ;; dummy-fun will tell us it was called if we do so accidentally
