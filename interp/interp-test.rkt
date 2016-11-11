38079533929f291a5d6c1e836504ee50ea946938
Chris <chris.cornelison@gmail.com>
2016-11-10 21:18:36 -0600

    Basic type checker

--- interp/interp-test.rkt
+++ interp/interp-test.rkt
@@ -3,114 +3,59 @@
 (require "interp.rkt")
 (print-only-errors true)
 
-;; test interp
-(define (interp-ast-res [x : ExprC]) : Result
-  (interp x mt-env mt-store))
+(define (tce [x : ExprC]) : Type
+  (tc x mt-env))
 
-(define (interp-ast-val [x : ExprC]) : Value
-  (v*s-v (interp x mt-env mt-store)))
+(test (numT) 
+      (tce (numC 42)))
 
-;(define-type Result
-;  [v*s (v : Value) (s : Store)])
+(test (numT)
+      (tce (plusC (numC 1)
+		  (numC 2))))
 
-(test (numV 42) 
-      (interp-ast-val (numC 42)))
+(test (numT)
+      (tce (multC (numC 1)
+		  (numC 2))))
 
-(test (numV 3)
-      (interp-ast-val (plusC (numC 1)
-			     (numC 2))))
+(test/exn (tce (multC (lamC 'x (numT) (numT) (numC 1))
+		      (numC 2)))
+	  "* type error")
 
-(test (numV 2)
-      (interp-ast-val (multC (numC 1)
-			     (numC 2))))
+(test/exn (tce (plusC (lamC 'x (numT) (numT) (numC 1))
+		      (numC 2)))
+	  "+ type error")
 
+(test (funT (numT) (numT)) 
+      (tce (lamC 'x 
+		 (numT) 
+		 (numT) 
+		 (plusC (idC 'x)
+			(idC 'x)))))
 
-; (+ 10 ((lambda (_) 5) 10))
-(test (numV 15)
-      (interp-ast-val (plusC (numC 10)
-			     (appC (lamC '_ (numC 5))
-				   (numC 10)))))
-
-; (lambda (x) ((lambda (y) (+ x y)) 4) 3)
-(test (numV 7) 
-      (interp-ast-val (appC (lamC 'x 
-				  (appC (lamC 'y 
-					      (plusC (varC 'x)
-						     (varC 'y)))
-					(numC 4)))
-			    (numC 3))))
-
-
-; we want to test something like this...
-;(let (x 42)
-;  (begin
-;    (set! x 43)
-;    x))
-; but we don't have 'let', so desugar to lambda...
-;((lambda (x) 
-;   (begin
-;     (set! x 43)
-;     x))
-; 42)
-; and test	 
-(test (numV 43)
-      (interp-ast-val 
-	(appC 
-	  (lamC 'x (seqC
-		     (setC 'x (numC 43))
-		     (varC 'x)))
-	  (numC 42))))
+(test (numT)
+      (tce (appC (lamC 'x (numT) (numT) (plusC (idC 'x)
+					       (idC 'x)))
+		 (numC 10))))
+			
+(test/exn (tce (appC (numC 1) (numC 10)))
+	  "non function type in function position")
 
 
-; test store
-(test/exn (fetch 0 mt-store) "location not found")
-(test (numV 42) (fetch 0 (override-store (cell 0 (numV 42)) mt-store)))
-(let ([sto (override-store (cell 0 (numV 42)) mt-store)])
-  (test (list (cell 0 (numV 43)) 
-	      (cell 0 (numV 42)))
-	(override-store (cell 0 (numV 43)) sto)))
+(test/exn (tce (appC (lamC 'x 
+			   (numT) 
+			   (numT) 
+			   (plusC (idC 'x) (idC 'x)))
+		 (lamC 'y (numT) (numT) (numC 10))))
+	  "app arg type mismatch")
+
+
+(test/exn (tce (lamC 'x
+		 (numT) 
+		 (numT) 
+		 (lamC 'y (numT) (numT) (numC 42))))
+      "lam type mismatch")
 
 ; test env
 (test/exn (lookup 'x mt-env) "name not found")
-(test 0 (lookup 'x (extend-env (bind 'x 0) mt-env)))
-(test 1 (lookup 'x (extend-env (bind 'x 1) 
-			       (extend-env (bind 'x 0) 
-					   mt-env))))
+(test (numT) (lookup 'x (extend-env (bind 'x (numT)) mt-env)))
 
-; test parser/interp
-; don't have syntax for box, unbox, setbox, begin, or let
-; in the parser, although begin and let could be desugared
-; to lambda's
-;(test (numC 42) 
-;      (parse '42))
-;(test (plusC (numC 1) (numC 2)) 
-;      (parse '(+ 1 2)))
-;(test (plusC (numC 1) (numC 2)) 
-;      (parse '(+ 1 2)))
-;(test (multC (plusC (numC 1) (numC 1)) 
-;	     (numC 2))
-;      (parse '(* (+ 1 1) 2)))
-;(test (multC (numC 1) 
-;	     (plusC (numC 2) (numC 2))) 
-;      (parse '(* 1 (+ 2 2))))
-;(test (lamC 'x (plusC (varC 'x) (varC 'x)))
-;      (parse '(lambda (x) (+ x x))))
-;(test (appC (lamC 'x (varC 'x)) (plusC (numC 1) (numC 1)))
-;      (parse '((lambda (x) x) (+ 1 1))))
-
-;; Exercise to desugar begin to lambda's
-;(begin
-;  (+ 1 2)
-;  (+ 3 4))
-;
-;; desugar to 'let'
-;(let (r1 (+ 1 2))
-;  (let (r2 (+ 3 4))
-;    r2))
-;
-;; desugar to 'lambda'
-;((lambda (r1)
-;   (lambda (r2)
-;     r2))
-; (+ 1 2)
-; (+ 3 4))
--- interp/interp.rkt
+++ interp/interp.rkt
@@ -1,134 +1,68 @@
 #lang plai-typed
 
-(define-type-alias Location number)
 
 (define-type Binding
-  [bind (name : symbol) (val : Location)])
+  [bind (name : symbol) (typ : Type)])
 
 (define-type-alias Env (listof Binding))
 (define mt-env empty)
 (define extend-env cons)
 
-(define-type Storage
-  [cell (location : Location) (val : Value)])
-
-(define-type-alias Store (listof Storage))
-(define mt-store empty)
-(define override-store cons)
-
-
-(define (lookup [n : symbol] [env : Env]) : Location
+(define (lookup [n : symbol] [env : Env]) : Type
   (cond
     [(empty? env) (error n "name not found")]
     [else (cond 
 	    [(symbol=? n (bind-name (first env))) 
-	     (bind-val (first env))]
+	     (bind-typ (first env))]
 	    [else (lookup n (rest env))])]))
-
-
-(define (fetch [loc : Location] [sto : Store]) : Value
-  (cond
-    [(empty? sto) (error 'loc "location not found")]
-    [else (cond
-	    [(equal? loc (cell-location (first sto)))
-	     (cell-val (first sto))]
-	    [else (fetch loc (rest sto))])]))
 
 
 (define-type ExprC
   [numC  (n : number)]
-  [varC   (s : symbol)]
+  [idC   (s : symbol)]
   [appC  (fun : ExprC) (arg : ExprC)]
   [plusC (l : ExprC) (r : ExprC)]
   [multC (l : ExprC) (r : ExprC)]
-  [lamC  (arg : symbol) (body : ExprC)]
-  [setC (var : symbol) (arg : ExprC)]
-  [seqC (b1 : ExprC) (b2 : ExprC)]
-  )
+  [lamC  (arg : symbol) (argT : Type) (retT : Type) (body : ExprC)] )
 
 (define-type Value
   [numV (n : number)]
   [closV (arg : symbol) (body : ExprC) (env : Env)])
 
-(define-type Result
-  [v*s (v : Value) (s : Store)])
 
-(define (interp [expr : ExprC] [env : Env] [sto : Store]) : Result
+(define-type Type
+  [numT]
+  [funT (arg : Type) (ret : Type)])
+
+
+(define (tc [expr : ExprC] [env : Env]) : Type
   (type-case ExprC expr
-    [numC  (n) (v*s (numV n) sto)]
-    [varC   (n) (v*s (fetch (lookup n env) sto) sto)]
-    [plusC (l r) (type-case Result (interp l env sto)
-		   [v*s (v-l s-l) 
-			(type-case Result (interp r env s-l)
-			  [v*s (v-r s-r) 
-			       (v*s (num+ v-l v-r) s-r)])])]
-    [multC (l r) (type-case Result (interp l env sto)
-	           [v*s (v-l s-l)
-			(type-case Result (interp r env s-l)
-			  [v*s (v-r s-r)
-			       (v*s (num* v-l v-r) s-r)])])]
-    [lamC  (a b) (v*s (closV a b env) sto)]
-    [setC (var val) (type-case Result (interp val env sto)
-		      [v*s (v-val s-val)
-			   (let ([where (lookup var env)]) ; get the l-value
-			     (v*s v-val
-				  (override-store (cell where v-val)
-						  s-val)))])]
-    [seqC (b1 b2) (type-case Result (interp b1 env sto)
-	            [v*s (v-b1 s-b1)
-			 (interp b2 env s-b1)])]
-    [appC (f a) (type-case Result (interp f env sto)
-		  [v*s (v-f s-f) 
-		       (type-case Result (interp a env s-f)
-		         [v*s (v-a s-a)
-			      (let ([where (new-loc)])
-				(interp (closV-body v-f)
-					(extend-env (bind (closV-arg v-f) where)
-						    (closV-env v-f))
-					(override-store (cell where v-a) s-a)))])])]
+    [numC  (n) (numT)]
+    [idC   (n) (lookup n env)]
+    [plusC (l r) (let ([lt (tc l env)]
+		       [rt (tc r env)])
+		   (if (and (equal? lt (numT))
+			    (equal? rt (numT)))
+		     (numT)
+		     (error 'tc "+ type error")))]
+    [multC (l r) (let ([lt (tc l env)]
+		       [rt (tc r env)])
+		   (if (and (equal? lt (numT))
+			    (equal? rt (numT)))
+		     (numT)
+		     (error 'tc "* type error")))]
+    [lamC (a argT retT b) 
+	  (if (equal? (tc b (extend-env (bind a argT) env)) retT)
+	    (funT argT retT)
+	    (error 'tc "lam type mismatch"))]
+    [appC (f a) (let ([ft (tc f env)]
+		      [at (tc a env)])
+		  (cond 
+		    [(not (funT? ft))
+		     (error 'tc "non function type in function position")]
+		    [(not (equal? (funT-arg ft) at))
+		     (error 'tc "app arg type mismatch")]
+		    [else (funT-ret ft)]))]
     ))
 
 
-(define (num+ [l : Value] [r : Value]) : Value
-  (cond
-    [(and (numV? l) (numV? r))
-     (numV (+ (numV-n l) (numV-n r)))]
-    [else
-      (error 'num+ "one argument was not a number")]))
-
-
-(define (num* [l : Value] [r : Value]) : Value
-  (cond
-    [(and (numV? l) (numV? r))
-     (numV (* (numV-n l) (numV-n r)))]
-    [else
-      (error 'num* "one argument was not a number")]))
-
-
-; This is cheating, relying on box to implement locations
-; for boxes. To eliminate, would have to thread the next
-; available location through the interpreter.
-(define new-loc
-  (let ([n (box 0)])
-    (lambda ()
-      (begin
-	(set-box! n (add1 (unbox n)))
-	(unbox n)))))
-
-
-;(define (parse [s : s-expression]) : ExprC
-;  (cond
-;    [(s-exp-number? s) (numC (s-exp->number s))]
-;    [(s-exp-symbol? s) (varC (s-exp->symbol s))]
-;    [(s-exp-list? s)
-;     (let ([sl (s-exp->list s)])
-;       (cond 
-;	 [(s-exp-list? (first sl))
-;	  (appC (parse (first sl)) (parse (second sl)))]
-;	 [else (case (s-exp->symbol (first sl))
-;		 [(+) (plusC (parse (second sl)) (parse (third sl)))]
-;		 [(*) (multC (parse (second sl)) (parse (third sl)))]
-;		 [(lambda) (lamC (s-exp->symbol (first (s-exp->list (second sl))))
-;				 (parse (third sl)))]
-;		 )]))]
-;    [else (error 'parse "invalid input")]))
